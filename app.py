259ff7ca427a1ae9de4e431cd38c0a1ea1089453
thoritie <thohirh9@gmail.com>
2019-11-17 00:55:49 +0700

    :tada: Initial commit :snowman:

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+from utils import fizzbuzz
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('homepage.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    number = request.form.get('number')
+
+    print(fizzbuzz(number))
+    say = fizzbuzz(number)
+
+    return render_template(
+        'result.html',
+        say=say
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
